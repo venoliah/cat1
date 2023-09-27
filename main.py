fe3df59d8733b1f4e8e0d33b0ca65261429c48e9
Kimson <kimwebbi006@gmail.com>
2023-09-27 16:38:13 +0300

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import os
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Specify the base directory where your folders are located
+base_directory = 'C:/Users/User/OneDrive/Desktop/today/1.1/data'  # Adjust the path accordingly
 
+# Initialize an empty list to store all data
+all_data = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Iterate through all folders in the base directory
+for folder_name in os.listdir(base_directory):
+    folder_path = os.path.join(base_directory, folder_name)
 
+    # Check if it's a directory
+    if os.path.isdir(folder_path):
+        # Iterate through all JSONL files in the folder
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.jsonl'):
+                jsonl_file_path = os.path.join(folder_path, filename)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+                # Initialize an empty list to store data from this file
+                data = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                # Open the JSONL file and read it line by line
+                with open(jsonl_file_path, 'r', encoding='utf-8') as file:
+                    for line in file:
+                        # Parse each line as a JSON object and append it to the data list
+                        json_object = json.loads(line)
+                        data.append(json_object)
+
+                # Append the data from this file to the all_data list
+                all_data.extend(data)
+
+# Now, 'all_data' contains a list of dictionaries from all JSONL files in all folders
