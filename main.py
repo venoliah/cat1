7c1ef1b70a5d1ca226975b17b84f6ebc8d9bb687
venoliah <venoliah.kennedy@strathmore.edu>
2023-10-03 23:06:12 +0300

    generation of separate json-l files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import os
 
+
 # Question 1
 folder_path = 'data'
 dfs = []
@@ -33,3 +34,35 @@ for index, language_file in enumerate(os.listdir(folder_path)):
 
 print("Excel files generated successfully.")
 
+# Question 2 (a)
+en_train = dfs[10][dfs[10]['partition']=='train']
+en_train.to_json('json/en_train.jsonl',orient='records',lines=True)
+
+en_test = dfs[10][dfs[10]['partition']=='test']
+en_test.to_json('json/en_test.jsonl',orient='records',lines=True)
+
+en_dev = dfs[10][dfs[10]['partition']=='dev']
+en_dev.to_json('json/en_dev.jsonl',orient='records',lines=True)
+
+
+de_train = dfs[8][dfs[8]['partition']=='train']
+de_train.to_json('json/de_train.jsonl',orient='records',lines=True)
+
+
+de_test = dfs[8][dfs[8]['partition']=='test']
+de_test.to_json('json/de_test.jsonl',orient='records',lines=True)
+
+de_dev = dfs[8][dfs[8]['partition']=='dev']
+de_dev.to_json('json/de_dev.jsonl',orient='records',lines=True)
+
+
+sw_train = dfs[42][dfs[42]['partition']=='train']
+sw_train.to_json('json/sw_train.jsonl',orient='records',lines=True)
+
+
+
+sw_test = dfs[42][dfs[42]['partition']=='test']
+sw_test.to_json('json/sw_test.jsonl',orient='records',lines=True)
+
+sw_dev = dfs[42][dfs[42]['partition']=='dev']
+sw_dev.to_json('json/sw_dev.jsonl',orient='records',lines=True)
